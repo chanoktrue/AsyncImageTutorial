0f86ad1a94c11ce62d182fed65cd646afeb26f4d
chanoktrue <chanoktrue@me.com>
2021-07-31 11:28:40 +0700

    up to github

---
+++ SwiftUI Tutorial/SwiftUI Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI Tutorial
+//
+//  Created by Thongchai Subsaidee on 31/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
